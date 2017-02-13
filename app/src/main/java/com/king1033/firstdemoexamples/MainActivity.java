b639db720ac418593a4efc2bdff2cc2557a555c5
King1033 <1786551559@qq.com>
2017-02-13 15:15:11 +0800

    First Demo Examples Unit1 First Update

--- app/src/main/java/com/king1033/firstdemoexamples/MainActivity.java
+++ app/src/main/java/com/king1033/firstdemoexamples/MainActivity.java
@@ -1,7 +1,9 @@
 package com.king1033.firstdemoexamples;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
     }
 }
